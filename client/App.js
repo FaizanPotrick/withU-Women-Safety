c42664997637a0adbec665f0321a6f9448c16a02
SarthakDeshmukh13 <saksard@gmail.com>
2023-04-19 02:58:01 +0530

    Loader added

--- client/App.js
+++ client/App.js
@@ -1,29 +1,44 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { StatusBar } from 'react-native'
-import { useFonts } from 'expo-font'
-import React, { useContext } from 'react'
-import Auth from './screens/auth/AuthScreen'
-import MainScreen from './screens/MainScreen'
-import StateContext, { StateProvider } from './context/StateContext'
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "react-native";
+import { useFonts } from "expo-font";
+import React, { useContext } from "react";
+import Auth from "./screens/auth/AuthScreen";
+import MainScreen from "./screens/MainScreen";
+import StateContext, {
+  StateProvider,
+  SocketProvider,
+} from "./context/StateContext";
+import Loading from "./screens/pages/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    'Poppins-Bold': require('./assets/Fonts/Poppins-Bold.ttf'),
-    'Poppins-Thin': require('.//assets/Fonts/Poppins-Thin.ttf'),
-    'Poppins-Medium': require('.//assets/Fonts/Poppins-Medium.ttf'),
-  })
-  if (!fontsLoaded) return null
+    "Poppins-Bold": require("./assets/Fonts/Poppins-Bold.ttf"),
+    "Poppins-Thin": require(".//assets/Fonts/Poppins-Thin.ttf"),
+    "Poppins-Medium": require(".//assets/Fonts/Poppins-Medium.ttf"),
+  });
+  if (!fontsLoaded) return null;
   return (
     <StateProvider>
-      <StatusBar barStyle={'light-content'} />
+      <StatusBar barStyle={"light-content"} />
       <NavigationContainer>
         <Provider />
       </NavigationContainer>
     </StateProvider>
-  )
+  );
 }
 
 const Provider = () => {
-  const { isLogin } = useContext(StateContext)
-  return <>{isLogin ? <MainScreen /> : <Auth />}</>
-}
+  const { isLogin } = useContext(StateContext);
+  return (
+    <>
+      {isLogin ? (
+        <SocketProvider>
+          <MainScreen />
+        </SocketProvider>
+      ) : (
+        <Auth />
+      )}
+      <Loading />
+    </>
+  );
+};
