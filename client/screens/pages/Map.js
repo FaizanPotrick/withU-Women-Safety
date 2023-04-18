5a234b9914adc66647f71ed58d7117a0abe00146
adi50920 <adityarai50920@gmail.com>
2023-04-19 01:43:45 +0530

    map page added

--- client/screens/pages/Map.js
+++ client/screens/pages/Map.js
@@ -1,12 +1,200 @@
-import { View, Text } from "react-native";
-import React from "react";
+import MapView, { Circle, Marker } from 'react-native-maps'
+import React, { useState, useEffect, useContext, useRef } from 'react'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+// import StateContext from '../../context/StateContext'
+// import { SERVER_URL } from '../../config'
 
 const Map = () => {
-  return (
-    <View>
-      <Text>Map</Text>
-    </View>
-  );
-};
+  // const { socket, setLoading, location, User } = useContext(StateContext)
+  // const [activeUsers, setActiveUsers] = useState([])
+  const [PoliceInfo, setPoliceInfo] = useState([])
+  const [SOSInfo, setSOSInfo] = useState([])
+  const [location, setLocation] = useState(null)
 
-export default Map;
+  // const Fetch_Active_Users = async () => {
+  //   const { data } = await axios.get(`${SERVER_URL}/api/active/location`)
+  //   setActiveUsers(data)
+  // }
+
+  // useEffect(() => {
+  //   setLoading(true)
+  //   Fetch_Active_Users()
+  //   setLoading(false)
+  // }, [socket.connected])
+
+  // useEffect(() => {
+  //   ;(async () => {
+  //     try {
+  //       const { data } = await axios.get(`${SERVER_URL}/api/police_sos`)
+  //       setPoliceInfo(data.police_response)
+  //       setSOSInfo(data.sos_response)
+  //     } catch (error) {
+  //       console.log(error)
+  //       alert(error)
+  //     }
+  //   })()
+  // }, [socket.connected])
+
+  const mapViewRef = useRef(null)
+
+  const relocateToUserLocation = () => {
+    mapViewRef.current.animateToRegion({
+      latitude: location.latitude,
+      longitude: location.longitude,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.01,
+    })
+  }
+
+  // useEffect(() => {
+  //   socket.on('Update_Active_Users', () => {
+  //     Fetch_Active_Users()
+  //   })
+  //   return () => {
+  //     socket.off('Update_Active_Users')
+  //   }
+  // }, [socket.connected])
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      (
+      <>
+        <TouchableOpacity
+          style={{
+            position: 'absolute',
+            bottom: 130,
+            right: 30,
+            backgroundColor: 'white',
+            width: 60,
+            height: 60,
+            borderRadius: 50,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            zIndex: 1,
+            shadowColor: '#000',
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+            elevation: 5,
+          }}
+          onPress={relocateToUserLocation}
+        >
+          <Image
+            source={require('../../assets/icons/precision.png')}
+            style={{ width: 30, height: 30 }}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+        <MapView
+          ref={mapViewRef}
+          style={{
+            width: '100%',
+            height: '100%',
+          }}
+          region={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01,
+          }}
+          provider="google"
+        >
+          {activeUsers.map((user, index) => {
+            return (
+              <Marker
+                key={index}
+                coordinate={user.coordinates}
+                opacity={user.user_id === User.user_id ? 1 : 0.6}
+                pinColor="red"
+              >
+                <View
+                  style={{
+                    borderWidth: 2,
+                    borderColor: 'black',
+                    borderRadius: 40,
+                  }}
+                >
+                  <Image
+                    source={require('../../assets/icons/woman.png')}
+                    style={{ width: 40, height: 40 }}
+                    resizeMode="contain"
+                  />
+                </View>
+              </Marker>
+
+              // <Marker
+              //   key={index}
+              //   coordinate={user.coordinates}
+              //   opacity={user.user_id === User.user_id ? 1 : 0.6}
+              // >
+              //   <Image
+              //     source={require('../../assets/placeholder.png')}
+              //     style={{ width: 40, height: 40 }}
+              //     resizeMode="contain"
+              //   />
+              // </Marker>
+            )
+          })}
+          {PoliceInfo.map((police, index) => {
+            console.log(police.type_of_user)
+            return (
+              <Marker
+                key={index}
+                title={police.branch_name}
+                coordinate={police.coordinates}
+              >
+                <View
+                  style={{
+                    borderWidth: 2,
+                    borderColor: 'black',
+                    borderRadius: 40,
+                  }}
+                >
+                  <Image
+                    source={
+                      police.type_of_user === 'police'
+                        ? require('../../assets/icons/police.png')
+                        : require('../../assets/icons/hospital.png')
+                    }
+                    style={{ width: 40, height: 40 }}
+                    resizeMode="contain"
+                  />
+                </View>
+              </Marker>
+            )
+          })}
+          {SOSInfo.map((sos, index) => {
+            return (
+              <Circle
+                center={sos.coordinates}
+                radius={120}
+                fillColor={'rgba(255,0,0,0.05)'}
+                strokeColor={'rgba(255,0,0,0.0)'}
+                strokeWidth={0}
+                key={index}
+              />
+            )
+          })}
+          <Circle
+            center={location}
+            radius={3000}
+            fillColor={'rgba(0,0,0,0.1)'}
+          />
+        </MapView>
+      </>
+      )
+    </View>
+  )
+}
+
+export default Map
