963d758674d6f2ea0b23c56dcea6263580c10269
Karan2310 <karandeep9619@gmail.com>
2023-04-19 04:04:16 +0530

    Stories connected to backend

--- client/screens/pages/AllStories.js
+++ client/screens/pages/AllStories.js
@@ -1,42 +1,38 @@
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  FlatList,
-  RefreshControl,
-} from "react-native";
-import React, { useState } from "react";
-import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
-
+import { View, Text, FlatList, RefreshControl, Alert } from "react-native";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { SERVER_URL } from "../../config";
 import CommonStyles from "../../CommonStyles";
+import Loading from "./Loading";
 
 const AllStories = () => {
-  const [modalVisible, setModalVisible] = useState(false);
-  const [data, setData] = useState([
-    {
-      _id: "1",
-      title: "My First Blog Post",
-      description:
-        "This is my first blog post. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      user: {
-        _id: "1",
-        email_address: "user1@example.com",
-      },
-    },
-    {
-      _id: "2",
-      title: "My Second Blog Post",
-      description:
-        "This is my second blog post. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      user: {
-        _id: "2",
-        email_address: "user2@example.com",
-      },
-    },
-  ]);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const GetAllStories = async () => {
+    setLoading(true);
+    try {
+      const { data } = await axios.get(`${SERVER_URL}/api/story`);
+      setData(data);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    GetAllStories();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <FlatList
-      refreshControl={<RefreshControl refreshing={false} />}
+      refreshControl={
+        <RefreshControl refreshing={loading} onRefresh={GetAllStories} />
+      }
       keyExtractor={(item, index) => index.toString()}
       showsVerticalScrollIndicator={false}
       style={{ padding: 30 }}
@@ -52,7 +48,11 @@ const AllStories = () => {
             <View style={CommonStyles.cardRow}>
               <View>
                 <Text style={CommonStyles.title}>{item.title}</Text>
-                <Text style={CommonStyles.silentText}>{item.description}</Text>
+                <View>
+                  <Text style={CommonStyles.silentText}>
+                    {item.description}
+                  </Text>
+                </View>
               </View>
             </View>
             <View style={CommonStyles.divider}></View>
