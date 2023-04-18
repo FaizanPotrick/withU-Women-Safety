1209bca8bbb8513bf137df22af543e69071a998a
Karan2310 <karandeep9619@gmail.com>
2023-04-18 18:42:01 +0530

    isLogin Switch Added

--- client/App.js
+++ client/App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import MainScreen from "./screens/MainScreen";
+import Auth from "./screens/auth/AuthScreen";
 
 export default function App() {
+  const isLogin = true;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={styles.container}>{isLogin ? <MainScreen /> : <Auth />}</View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ client/screens/auth/AuthScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+function AuthScreen() {
+  return <Text>AuthScreen</Text>;
+}
+
+export default AuthScreen;
