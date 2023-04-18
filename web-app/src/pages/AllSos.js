fc38ddecb88bee90b717b50bca448e4b693da489
SarthakDeshmukh13 <saksard@gmail.com>
2023-04-19 04:57:59 +0530

    Folder structure created and login page added for web

---
+++ web-app/src/pages/AllSos.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AllSos() {
+  return <div>AllSos</div>;
+}
+
+export default AllSos;
---
+++ web-app/src/pages/MainScreen/LightDark.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LightDark() {
+  return <div>LightDark</div>;
+}
+
+export default LightDark;
